dbfbe57461abc5f83ca0f99990abae67024e2355
Santiago dos Santos <santiiagodossantos@icloud.com>
2024-07-15 20:43:54 -0300

    Mocks and code

--- projects/05-movies-search/README.md
+++ projects/05-movies-search/README.md
@@ -2,6 +2,7 @@ Enunciado
 Crea una aplicación para buscar películas
 
 API a usar: - https://www.omdbapi.com/ Consigue la API Key en la propia página web registrando tu email.
+My API KEY: 9d243264
 
 Requerimientos:
 
--- projects/05-movies-search/src/App.jsx
+++ projects/05-movies-search/src/App.jsx
@@ -3,9 +3,20 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='page'>
+     <header>
+        <h1>Buscador de Películas</h1>
+        <form className='form'>
+          <input type="text" placeholder='Avengers, Star Wars...' />
+          <button type='submit'>Buscar</button>
+        </form>
+     </header>
+
+    <main>
+
+    </main>
+
+    </div>
   )
 }
 
