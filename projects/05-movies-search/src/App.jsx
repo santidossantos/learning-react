52d73801e6b0ff73c60f7334c553542e8ac97155
Santiago dos Santos <santiiagodossantos@icloud.com>
2024-07-15 21:27:02 -0300

    Movies map

--- projects/05-movies-search/src/App.jsx
+++ projects/05-movies-search/src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css'
+import responseMovies from './mocks/with-results.json'
+import withoutResults from './mocks/no-results.json'
 
 function App () {
+  const movies = responseMovies?.Search
+  const hasMovies = movies?.length > 0
+
   return (
     <div className='page'>
+
       <header>
         <h1>Buscador de Películas</h1>
         <form className='form'>
@@ -11,7 +17,27 @@ function App () {
         </form>
       </header>
 
-      <main />
+      <main>
+
+        {hasMovies
+          ? (
+            <ul>
+              {
+               movies.map((movie) => (
+                 <li key={movie.imdbID}>
+                   <img src={movie.Poster} alt={movie.Title} />
+                   <h2>{movie.Title}</h2>
+                   <p>{movie.Year}</p>
+                 </li>
+               ))
+            }
+            </ul>
+            )
+          : (
+            <p>No se encontraron resultados</p>
+            )}
+
+      </main>
 
     </div>
   )
