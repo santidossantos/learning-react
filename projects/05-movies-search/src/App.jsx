dfc0ac42e9e34d61db428dc04b05c5900693d559
Santiago dos Santos <santiiagodossantos@icloud.com>
2025-01-20 13:30:18 -0300

    Input useRef

--- projects/05-movies-search/src/App.jsx
+++ projects/05-movies-search/src/App.jsx
@@ -1,27 +1,37 @@
-import './App.css'
-import { Movies } from './components/Movies'
-import { useMovies } from './hooks/useMovies'
+import { useRef } from "react";
+import "./App.css";
+import { Movies } from "./components/Movies";
+import { useMovies } from "./hooks/useMovies";
 
-function App () {
-  const { movies } = useMovies()
+function App() {
+  const { movies } = useMovies();
+  const inputRef = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const input = inputRef.current.value;
+    console.log(input);
+  }
 
   return (
-    <div className='page'>
-
+    <div className="page">
       <header>
         <h1>Buscador de Películas</h1>
-        <form className='form'>
-          <input type='text' placeholder='Avengers, Star Wars...' />
-          <button type='submit'>Buscar</button>
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            ref={inputRef}
+            type="text"
+            placeholder="Avengers, Star Wars..."
+          />
+          <button type="submit">Buscar</button>
         </form>
       </header>
 
       <main>
         <Movies movies={movies} />
       </main>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
